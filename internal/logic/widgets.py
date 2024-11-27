c2a16f558eb7a8ada6f894cc3d19697a5984008f
Tikhon2783 <159179679+Tikhon2783@users.noreply.github.com>
2024-11-28 00:37:10 +0300

    Доброе утро! (Немного логики и заготовок на будущее, изменение цвета фона ui)

--- internal/logic/widgets.py
+++ internal/logic/widgets.py
@@ -1,15 +1,19 @@
 from PyQt6 import uic  # Импортируем uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QWidget
 
-
+# Виджет окна входа в аккаунт
 class AuthWidget(QMainWindow):
-    def __init__(self):
+    def __init__(self, next, *next_args):
         super().__init__()
         uic.loadUi('internal/ooi/aooth.ui', self)  # Загружаем дизайн
+
+        self.next_widget = next
+        self.next_args = next_args
 
         self.lbl_username.textChanged.connect(self.check_labels)
         self.lbl_psw.textChanged.connect(self.check_labels)
     
+    # Проверка на заполнение полей перед активацией кнопок входа
     def check_labels(self):
         if self.lbl_username.text() and self.lbl_psw.text():
             self.btn_login.setEnabled(True)
@@ -17,3 +21,71 @@ class AuthWidget(QMainWindow):
         else:
             self.btn_login.setEnabled(False)
             self.btn_signup.setEnabled(False)
+    
+    # Метод проверки пароля
+    def authenticate(self, username, password):
+        # todo: Query DB
+        todo_verified = (password == username + '1')
+        if not todo_verified:
+            # todo: show error widget
+            return
+        
+        # Пользователь прошел проверку аутентификации
+        self.grant_access()
+    
+    # Метод на регистрацию пользователя
+    def register(self, username, password):
+        # todo: Query DB
+        todo_username_taken = True
+        if todo_username_taken:
+            # todo: show error widget
+            return
+        
+        # Пользователь успешно зарегистрирован
+        self.grant_access()
+    
+    # Метод успешного входа в аккаунт
+    def grant_access(self):
+        # todo: simple loading screen widget
+        # todo: welcome back message with csv
+
+        # Открываем следующий виджет и закрываем текущий
+        self.next(*self.next_args)
+        self.destroy()
+
+
+# Виджет главного меню
+class MainMenuWidget(QWidget):
+    def __init__(self, info_widget, leaderboard_widget, *games_widgets):
+        super().__init__()
+        raise "UI FILE DOES NOT EXISTS YET!"
+        uic.loadUi('internal/ooi/meenyoo.ui', self)  # Загружаем дизайн
+
+        self.nextWidget = next
+               
+        self.lbl_username.textChanged.connect(self.check_labels)
+        self.lbl_psw.textChanged.connect(self.check_labels)
+
+
+# Виджет окна "О программе"
+class ProgramInfoWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        raise "UI FILE DOES NOT EXISTS YET!"
+        uic.loadUi('internal/ooi/eenfoo.ui', self)  # Загружаем дизайн
+
+
+# Виджет окна с результатами и достижениями
+class LeaderboardWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        raise "UI FILE DOES NOT EXISTS YET!"
+        uic.loadUi('internal/ooi/leedeerbhoord.ui', self)  # Загружаем дизайн
+
+
+# Виджет окна с игрой blackjack (двадцать одно)
+class BlackJackWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        raise "UI FILE DOES NOT EXISTS YET!"
+        uic.loadUi('internal/ooi/blakejake.ui', self)  # Загружаем дизайн
--- main.py
+++ main.py
@@ -1,72 +1,22 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QMessageBox
-from PyQt6.QtGui import QPixmap
-from PyQt6 import uic
 import sys
-import random
+from PyQt6.QtWidgets import QApplication
+import internal.logic.widgets as wdt
+from internal.logic.logging import logger
 
 
-class UI(QMainWindow):
-    def __init__(self):
-        super(UI, self).__init__()
-
-        # Load the ui file
-        uic.loadUi("blackjack.ui", self)
-        self.setWindowTitle("BlackJack!")
-
-        # Define our widgets
-        self.dealerCard1 = self.findChild(QLabel, "dealerCard1")
-        self.dealerCard2 = self.findChild(QLabel, "dealerCard2")
-        self.dealerCard3 = self.findChild(QLabel, "dealerCard3")
-        self.dealerCard4 = self.findChild(QLabel, "dealerCard4")
-        self.dealerCard5 = self.findChild(QLabel, "dealerCard5")
-
-        self.playerCard1 = self.findChild(QLabel, "playerCard1")
-        self.playerCard2 = self.findChild(QLabel, "playerCard2")
-        self.playerCard3 = self.findChild(QLabel, "playerCard3")
-        self.playerCard4 = self.findChild(QLabel, "playerCard4")
-        self.playerCard5 = self.findChild(QLabel, "playerCard5")
-
-        self.dealerHeaderLabel = self.findChild(QLabel, "dlabel")
-        self.playerHeaderLabel = self.findChild(QLabel, "plabel")
-
-        self.shuffleButton = self.findChild(QPushButton, "spushButton")
-        self.hitMeButton = self.findChild(QPushButton, "hitMeButton")
-        self.standButton = self.findChild(QPushButton, "standButton")
-
-        # Shuffle Cards
-        self.shuffle()
-
-        # Click Buttons
-        self.shuffleButton.clicked.connect(self.shuffle)
-        self.hitMeButton.clicked.connect(self.playerTakesCard)
-        self.standButton.clicked.connect(self.stand)
-
-        # Show The App
-        self.show()
-
-    # Stand
-    def stand(self):
-       pass
-
-    # Check For Blackjack
-    def blackjack(self, player):
-        pass
-
-    def shuffle(self):
-       pass
-
-    def dealCards(self):
-        pass
-
-    def dealerTakesCard(self):
-        pass
-
-    def playerTakesCard(self):
-        pass
-
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 if __name__ == '__main__':
+    # Ловим ошибки
+    sys.excepthook = except_hook
+
     app = QApplication(sys.argv)
-    ex = UI()
-    ex.show()
+    # Создаем экземпляры всех нужных классов и передаем их друг другу через аргументы
+    # Так, например, класс окна аутентификации (AuthWidget) будет вызывать метод next.do() аргумента next,
+    # где next - экземпляр класса виджета главного меню (MainMenuWidget)
+    auther = wdt.AuthWidget(wdt.MainMenuWidget, wdt.ProgramInfoWidget, wdt.LeaderboardWidget, wdt.BlackJackWidget)
+    auther.show()
+
     sys.exit(app.exec())
+    
